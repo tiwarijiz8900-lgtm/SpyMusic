1e79541e37d6bd03179fa7aaa71bb8edfe3f5a31
𝐒𝐇𝐄𝐇𝐙𝐀𝐃𝐀 𝐅𝐔𝐂𝐊𝐄𝐑 <tiwarijiz8900@gmail.com>
2025-12-31 20:37:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,10 +86,10 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "https://files.catbox.moe/ea5xq9.jpg"
+    "https://files.catbox.moe/r3n3ok.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/ea5xq9.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/i4aiar.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/e68f585a0a2e4e5dc1164.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/4a7c28726502e24ea0fe0.jpg"

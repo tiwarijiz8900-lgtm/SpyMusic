cf408bfa3ff7a9468b2c63452c95c2578d7cf92d
𝐒𝐇𝐄𝐇𝐙𝐀𝐃𝐀 𝐅𝐔𝐂𝐊𝐄𝐑 <tiwarijiz8900@gmail.com>
2026-01-12 14:33:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Love_Bot_143")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+qEkrEQlft5E0NWI1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Love_Ki_Duniyaa")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")

e7a58dc359498b17dd17a038abd2a28835061be8
𝐒𝐇𝐄𝐇𝐙𝐀𝐃𝐀 𝐅𝐔𝐂𝐊𝐄𝐑 <tiwarijiz8900@gmail.com>
2026-01-24 10:45:48 +0530

    Update start.py

--- Spy/plugins/bot/start.py
+++ Spy/plugins/bot/start.py
@@ -25,7 +25,7 @@ from config import BANNED_USERS
 from strings import get_string
 
 
-START_IMG_URL = "https://files.catbox.moe/03tmha.jpg"
+START_IMG_URL = "https://files.catbox.moe/wvsxti.jpg"
 
 
 VALID_EMOJII = [
@@ -179,5 +179,6 @@ async def welcome(client, message: Message):
 
         except Exception as e:
             print(e)
+
 
 

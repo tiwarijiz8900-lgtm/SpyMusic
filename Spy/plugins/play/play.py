ca26b77b4be9c20500b2a7d44f36683ef21f1383
𝐒𝐇𝐄𝐇𝐙𝐀𝐃𝐀 𝐅𝐔𝐂𝐊𝐄𝐑 <tiwarijiz8900@gmail.com>
2026-02-15 22:39:48 +0530

    Update play.py

--- Spy/plugins/play/play.py
+++ Spy/plugins/play/play.py
@@ -24,10 +24,7 @@ from Spy.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
-EMOJII = ["🔥", "💋", "🥺", "😒", "💖",
-          "💘", "💕", "✨", "🧪", "🥰",
-          "🚩", "🍌", "🫦", "💔", "🦠",
-          "😓", "🫧"]
+EMOJII = ["🥀 𝐏ɤσƈɛssɩŋʛ... 🦋"]
 
 @app.on_message(
     filters.command(
@@ -668,3 +665,4 @@ async def slider_queries(client, CallbackQuery, _):
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
         )
+

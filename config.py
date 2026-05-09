a955e3af83382c143c11d68b122885fb6b569259
𝐒𝐇𝐄𝐇𝐙𝐀𝐃𝐀 𝐅𝐔𝐂𝐊𝐄𝐑 <tiwarijiz8900@gmail.com>
2026-05-09 22:23:57 +0530

    Update support channel and chat URLs

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Love_Bot_143")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Love_Ki_Duniyaa")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_Rolex_support_ll")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+uB7rH5ns_QI4YzM1")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
